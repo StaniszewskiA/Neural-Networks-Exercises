9af99fa113323e348b02337e17624d4b54f3bbb0
AStaniszewski <staniszewski01adam@gmail.com>
2023-10-30 17:46:11 +0100

    CNNH and DHN

---
+++ Deep Hashing/dhn.py
@@ -0,0 +1,102 @@
+from utils import *
+from network import *
+
+import os
+import torch
+import torch.optim as optim
+import time
+import numpy as np
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+def get_config():
+    if torch.cuda.is_available():
+        device = torch.device("cuda:1")
+    else:
+        device = torch.device("cpu")
+
+    config = {
+        "alpha": 0.1,
+        "optimizer": {"type": optim.RMSprop, "optim_params": {"lr": 1e-5, "weight_decay": 10 ** -5}},
+        "info": "[DHN]",
+        "resize_size": 256,
+        "crop_size": 224,
+        "batch_size": 128,
+        "net": AlexNet,
+        "dataset": "cifar10-1",
+        "epoch": 90,
+        "test_map": 15,
+        "save_path": "save/DHN",
+        "device": device,
+        "bit_list": [48],
+    }
+    config = config_dataset(config)
+    return config
+
+class DHNLoss(torch.nn.Module):
+    def __init__(self, config, bit):
+        super(DHNLoss, self).__init__()
+        self.U = torch.zeros(config["num_train"], bit).float().to(config["device"])
+        self.Y = torch.zeros(config["num_train"], config["n_class"]).float().to(config["device"])
+
+    def forward(self, u, y, ind, config):
+        self.U[ind, :] = u.data
+        self.Y[ind, :] = y.float()
+
+        s = (y @ self.Y.t() > 0).float()
+        inner_product = u @ self.U.t() * 0.5
+
+        likelihood_loss = (1 + (-inner_product.abs()).exp()).log() + inner_product.clamp(min=0) - s * inner_product
+        likelihood_loss = likelihood_loss.mean()
+
+        quantization_loss = config["alpha"] * (u.abs() - 1).cosh().log().mean()
+
+        return likelihood_loss + quantization_loss
+
+def train_val(config, bit):
+    device = config["device"]
+    train_loader, test_loader, dataset_loader, num_train, num_dataset = get_data(config)
+    config["num_train"] = num_train
+    net = config["net"](bit).to(device)
+
+    optimizer = config["optimizer"]["type"](net.parameters(), **(config["optimizer"]["optim_params"]))
+
+    criterion = DHNLoss(config, bit)
+
+    Best_mAP = 0
+
+    for epoch in range(config["epoch"]):
+        current_time = time.strftime('%H:%M:%S', time.localtime(time.time()))
+
+        print("%s[%2d/%2d][%s] bit:%d, dataset:%s, training...." % (
+            config["info"], epoch + 1, config["epoch"], current_time, bit, config["dataset"]), end="")
+
+        net.train()
+
+        train_loss = 0
+        for image, label, ind in train_loader:
+            iamge = image.to(device)
+            label = label.to(device)
+
+            optimizer.zero_grad()
+            u = net(image)
+
+            loss = criterion(u, label.float(), ind, config)
+            train_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+        train_loss = train_loss / len(train_loader)
+
+        print("\b\b\b\b\b\b\b loss:%.3f" % (train_loss))
+
+        if (epoch + 1) % config["test_map"] == 0:
+            Best_mAP = validate(config, Best_mAP, test_loader, dataset_loader, net, bit, epoch, num_dataset)
+
+if __name__ == "__main__":
+    config = get_config()
+    print(config)
+    for bit in  config["bit_list"]:
+        config["pr_curve_path"] = f"log/alexnet/DHN_{config['dataset']}_{bit}.json"
+        train_val(config, bit)
